a2ca43365ee8e164b9ba4118af9ffffeae985e9f
Belen Saavedra <saavedrariosm@berea.edu>
2023-12-06 07:27:10 -0500

    added comments

---
+++ agora_code/lib/login_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  const LoginPage ({Key? key}): super (key:key);
+
+  @override
+  State<LoginPage> createState()==> _LoginPageState()
+}
+
+class _LoginPageState extends State<LoginPage>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body:Column(children:[
+
+      //  Welcome Message
+        Text('Welcome to Agora!', 
+        style:TextStyle(
+          fontWeight: FontWeight.bold, 
+          fontSize:24,
+          ),
+        ),
+
+        SizedBox(height: 10),
+         Text('Welcome back', 
+        style:TextStyle( 
+          fontSize:24,
+          ),
+        ),
+      // email field 
+
+      // password textfield 
+
+ // comment 
+      // sign in button 
+
+     ]),  
+    );
+  }
+}
